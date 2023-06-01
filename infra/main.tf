5ef342f7b63299858f629f7f1d6ef30002de7616
Ashiq Rahman <0xashiqrahman@gmail.com>
2023-06-01 22:53:26 +0530

    s3 static bucket create testing

---
+++ infra/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  cloud {
+    organization = "ashiqrahmantesting"
+
+    workspaces {
+      name = "github_actions"
+    }
+  }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.0.1"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "mybucket" {
+  bucket = "cmcloudlab520.info"
+
+  tags = {
+    Name        = "My Bucket"
+    Environment = "Dev"
+  }
+}
